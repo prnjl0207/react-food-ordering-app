de7f98cb4ad67b55d8e0a7372031a210c697f718
prnjl0207 <146756631+prnjl0207@users.noreply.github.com>
2023-10-12 16:59:17 +0000

    Update ListItems.js

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,6 +1,7 @@
 import { RESTRO_IMG_URL } from "../utils/constants";
 
 const ListItems = ({ items }) => {
+  console.log(items);
   return (
     <div>
       {items.map((item) => {
@@ -28,6 +29,9 @@ const ListItems = ({ items }) => {
                 ₹
                 {item?.card?.info?.price / 100 ||
                   item?.card?.info?.defaultPrice / 100}
+              </div>
+              <div className="text-sm text-slate-500 pr-[5%] pt-[1%]">
+                {item?.card?.info?.description}
               </div>
             </div>
             <div className="w-[118px] h-[96px] relative">
